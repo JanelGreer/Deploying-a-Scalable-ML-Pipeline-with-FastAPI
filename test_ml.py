518b9c5f2a97a561db60ea5922c753083529b6f1
Janel Greer <janelrgreer@gmail.com>
2025-03-28 19:30:19 -0400

    Update test_ml with unit test

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,40 @@
 import pytest
+from sklearn.ensemble import RandomForestClassifier
+from ml.model import train_model, inference, compute_model_metrics
+from train_model import X_train, y_train, X_test, y_test
 # TODO: add necessary import
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_output():
     """
-    # add description for the first test
+    # Test that train_model returns a trained model object.
     """
     # Your code here
-    pass
+    model = train_model(X_train, y_train)
+    assert model is not None
+    assert hasattr(model, "predict")
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_metrics():
     """
-    # add description for the second test
+    # Test that returns expected metric types.
     """
     # Your code here
-    pass
+    model = train_model(X_train, y_train)
+    prediction = inference(model, X_test)
+    precision, recall, f1 = compute_model_metrics(y_test, prediction)
+    assert isinstance(precision, float)
+    assert isinstance(recall, float)
+    assert isinstance(f1, float)
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_length():
     """
-    # add description for the third test
+    # Test if output length matches input sample size.
     """
     # Your code here
-    pass
+    model = train_model(X_train, y_train)
+    prediction = inference(model, X_train)
+    assert len(prediction) == len(X_train), "Inference output should match the number of input samples"
